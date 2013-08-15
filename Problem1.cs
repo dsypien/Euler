461e446d0e5eba729e00dd76d95db566bd29e633
dsypien <davidsypien@gmail.com>
2013-08-14 23:31:48 -0400

    Checking in Euler Project, First problem solved

---
+++ Problem1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler
+{
+    static class Problem1
+    {
+        public static int Run()
+        {
+            int sum = 0;
+
+            for (int i = 3; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Format("Euler Problem 1 result \t {0}", Problem1.Run()));
+            Console.ReadLine();
+        }
+    }
+}
