3ae69fc7cfb8c16ed6ecb854ee0d9fb5546cdd18
dsypien <davidsypien@gmail.com>
2013-08-15 00:01:22 -0400

    Euler Problem 2 Added, Even Fibonacci Numbers

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace Euler
         static void Main(string[] args)
         {
             Console.WriteLine(String.Format("Euler Problem 1 result \t {0}", Problem1.Run()));
+            Console.WriteLine(String.Format("Euler Problem 2 result \t {0}", Problem2.Run()));
+
             Console.ReadLine();
         }
     }
