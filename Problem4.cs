894b96b8c4e91f916ea6b927e6f838d578a541d7
dsypien <davidsypien@gmail.com>
2013-08-15 00:32:25 -0400

    Largest Palindrome Product ( Problem 4 )

--- Problem4.cs
+++ Problem4.cs
@@ -5,7 +5,46 @@ using System.Text;
 
 namespace Euler
 {
+    /// <summary>
+    /// Largest palindrome product
+    /// </summary>
+    /// 
+    /// A palindromic number reads the same both ways. The largest palindrome made 
+    /// from the product of two 2-digit numbers is 9009 = 91 × 99.
+    /// 
+    /// Find the largest palindrome made from the product of two 3-digit numbers.
+    
     class Problem4
     {
+        public static int Run()
+        {
+            int largestPalindromeProd = 0;
+
+            for (int i = 999; i > 100; i--)
+            {
+                for (int j = 999; j > 100; j--)
+                {
+                    var product = i * j;
+
+                    if (IsPalindrome(product.ToString()))
+                    {
+                        largestPalindromeProd = product > largestPalindromeProd ? product : largestPalindromeProd;
+                    }
+                }
+            }
+
+            return largestPalindromeProd;
+        }
+
+        private static bool IsPalindrome(string txt)
+        {
+            for (int i = 0, j = txt.Length - 1; i <= j; i++, j--)
+            {
+                if (txt[i] != txt[j])
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace Euler
         {
             Console.WriteLine(String.Format("Euler Problem 1 result \t {0}", Problem1.Run()));
             Console.WriteLine(String.Format("Euler Problem 2 result \t {0}", Problem2.Run()));
+            Console.WriteLine(String.Format("Euler Problem 4 result \t {0}", Problem4.Run()));
 
             Console.ReadLine();
         }
