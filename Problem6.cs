53ad98661c388ca5161b8cee1b8087a719f9a1e9
dsypien <davidsypien@gmail.com>
2013-08-16 06:42:36 -0400

    Problem # 6, Sum Square Differences

---
+++ Problem6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler
+{
+    public static class Problem6
+    {
+        public static double Run()
+        {
+            double sumOfSquares = 0;
+            double squareOfSums = 0;
+
+            for (double i = 1; i <= 100; i++)
+            {
+                sumOfSquares += Math.Pow(i, 2.0);
+            }
+
+            for (double i = 1; i <= 100; i++)
+            {
+                squareOfSums += i;
+            }
+
+            squareOfSums = Math.Pow(squareOfSums, 2.0);
+
+            return squareOfSums - sumOfSquares;
+        }
+    }
+}
