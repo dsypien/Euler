7bac75337845f4ef4bc8a7d968cfec9983af5442
dsypien <davidsypien@gmail.com>
2013-08-20 17:38:27 -0400

    sum of all primes below 2 million, problem 10

---
+++ Problem10.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Euler
+{
+    /// <summary>
+    /// Summation of primes
+    /// </summary>
+    ///
+    /// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+    /// 
+    /// Find the sum of all the primes below two million.
+    
+    public static class Problem10
+    {
+        public static long Run()
+        {
+            BitArray array = new BitArray(int.MaxValue, true);
+            long sum = 0;
+
+            for (int i = 2; i < 2000000; i++)
+            {
+                if (array[i] == true)
+                {
+                    sum += i;
+
+                    for (int j = i; j < 2000000; j += i)
+                    {
+                        array[j] = false;
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,15 +9,16 @@ namespace Euler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(String.Format("Euler Problem 1 result \t {0}", Problem1.Run()));
-            Console.WriteLine(String.Format("Euler Problem 2 result \t {0}", Problem2.Run()));
-            Console.WriteLine(String.Format("Euler Problem 3 result \t {0}", Problem3.Run()));
-            Console.WriteLine(String.Format("Euler Problem 4 result \t {0}", Problem4.Run()));
-            Console.WriteLine(String.Format("Euler Problem 5 result \t {0}", Problem5.Run()));
-            Console.WriteLine(String.Format("Euler Problem 6 result \t {0}", Problem6.Run()));
-            Console.WriteLine(String.Format("Euler Problem 7 result \t {0}", Problem7.Run()));
-            Console.WriteLine(String.Format("Euler Problem 8 result \t {0}", Problem8.Run()));
-            Console.WriteLine(String.Format("Euler Problem 9 result \t {0}", Problem9.Run()));
+            Console.WriteLine(String.Format("Euler Problem 1 result \t\t {0}", Problem1.Run()));
+            Console.WriteLine(String.Format("Euler Problem 2 result \t\t {0}", Problem2.Run()));
+            Console.WriteLine(String.Format("Euler Problem 3 result \t\t {0}", Problem3.Run()));
+            Console.WriteLine(String.Format("Euler Problem 4 result \t\t {0}", Problem4.Run()));
+            Console.WriteLine(String.Format("Euler Problem 5 result \t\t {0}", Problem5.Run()));
+            Console.WriteLine(String.Format("Euler Problem 6 result \t\t {0}", Problem6.Run()));
+            Console.WriteLine(String.Format("Euler Problem 7 result \t\t {0}", Problem7.Run()));
+            Console.WriteLine(String.Format("Euler Problem 8 result \t\t {0}", Problem8.Run()));
+            Console.WriteLine(String.Format("Euler Problem 9 result \t\t {0}", Problem9.Run()));
+            Console.WriteLine(String.Format("Euler Problem 10 result \t {0}", Problem10.Run()));
 
             Console.ReadLine();
         }
